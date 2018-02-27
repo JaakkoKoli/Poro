16f1dfa416e35b956e8574adfed55dd5dcc888ee
8Cookie9 <Cookie3141592653589>
2018-02-27 03:52:12 +0200

    authtest

--- porosite/src/models/statchange.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const StatChange = mongoose.model('StatChange', {
-    stat: String,
-    flat: Boolean,
-    amount: Number
-})
-
-module.exports = StatChange
---
+++ twitchauthtest/src/index.js
@@ -0,0 +1,57 @@
+const axios = require('axios')
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+app.get('/', async (request, response) => {
+    if(request.query.code){
+        const code = request.query.code
+        const req='https://api.twitch.tv/api/oauth2/token?client_id=pho06vchppcq16eknimsbqagdxkxya&client_secret=q5g6fchyrknt58zamrfo1nabsh0giz&code='+code+'&grant_type=authorization_code&redirect_uri=http://localhost:3001'
+        axios.post(req)
+            .then(res=>{
+              console.log(res.data)
+              if(res.data.access_token){
+                const config = {
+                  "headers": {
+                    "Authorization": 'OAuth '+res.data.access_token,
+                    "Accept": 'application/vnd.twitchtv.v5+json',
+                    "Client-ID": 'pho06vchppcq16eknimsbqagdxkxya'
+                  }
+                }
+                axios.get('https://api.twitch.tv/kraken', config)
+                  .then(r => {
+                    console.log(r.data)
+                    console.log("ID: "+r.data.token.user_id+"; NAME: "+r.data.token.user_name)
+                    if(!r.data.token.valid && !r.data.valid){
+                      axios.post('https://api.twitch.tv/kraken/oauth2/token?grant_type=refresh_token&refresh_token='+res.data.refresh_token+'&client_id=pho06vchppcq16eknimsbqagdxkxya&client_secret=q5g6fchyrknt58zamrfo1nabsh0giz')
+                        .then(r2 => {
+                          console.log(r2.data)
+                        })
+                        .catch(e => console.log(e))
+                    }
+                  })
+                  .catch(err => console.log(err))
+              }
+            })
+            .catch(res=>console.log(res))
+    }
+    response.send('<div><a href="https://api.twitch.tv/kraken/oauth2/authorize?client_id=pho06vchppcq16eknimsbqagdxkxya&redirect_uri=http://localhost:3001&response_type=code">authorise</a></div>')
+})
+
+
+const server = http.createServer(app)
+
+const port = 3001
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
+
+module.exports = {
+  app, server
+}
